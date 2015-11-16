85fbc39041e3f9691f39e695a1acd1df2b50a7b0
David Strawbridge <davidstrawbridge@golfstrike.co.uk>
2015-11-16 21:58:50 +0000

    added (non-compiling) test method

--- ContainerMess/Tests.cs
+++ ContainerMess/Tests.cs
@@ -6,6 +6,15 @@ namespace ContainerMess {
     public class Tests {
         [TestMethod]
         public void TestMethod1() {
+
+            var client = new Client();
+
+            // this does NOT work due to constraints
+
+            var container = new MyContainer();
+            var content = new ContentA();
+
+            client.DoSomething<MyContainer, ContentA>(container, content);
         }
     }
 }
