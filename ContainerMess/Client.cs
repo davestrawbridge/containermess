7805c4047a3d66a3110dbca7f4c658ae9e81b339
David Strawbridge <davidstrawbridge@golfstrike.co.uk>
2015-11-16 21:56:05 +0000

    implemented client for object model

---
+++ ContainerMess/Client.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContainerMess {
+    class Client {
+         
+        public void DoSomething<TContainer, TContent>(TContainer container, TContent content) 
+            where TContainer : ItemContainer<TContent> 
+            where TContent : Item {
+
+            var location = container.Location;
+
+            container.Remove(content);
+
+            content.Location = location;
+        }
+    }
+}
